b91afae397f21924a4f42c7f1f6622cc116ffdc2
Nick Griffith <nhgrif@gmail.com>
2015-03-21 10:57:54 -0500

    Added example usage.

--- UICheckBox/ViewController.swift
+++ UICheckBox/ViewController.swift
@@ -10,16 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func checkChanged(sender: UICheckbox) {
+        if sender.checked {
+            println("checkmark added")
+        } else {
+            println("checkmark removed")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
